f51ad7647db6698e852810bef6f901eb57edea9b
iva <ivalulyanov1@gmail.com>
2024-11-12 00:15:45 +0300

    Feat: add new feature - save news for user

--- app/backend/news/routers.py
+++ app/backend/news/routers.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 
 from auth.dependencies import get_db_session
 from db.database import News, Users, Users_News
+from news.schemas import User_News
 
 router = APIRouter(
     tags=["Новости"],
@@ -15,3 +16,16 @@ router = APIRouter(
 async def get_news(db: Session = Depends(get_db_session)):
     all_news = db.query(News).all()
     return all_news
+
+
+@router.post("/save", response_model=User_News)
+async def save_news(user_news: User_News, db: Session = Depends(get_db_session)):
+
+    save_data = Users_News(user_id=user_news.user_id, news_id=user_news.news_id)
+    db.add(save_data)
+    db.commit()
+    db.refresh(save_data)
+
+    user_news.msg = "Статья сохранена."
+
+    return user_news
--- app/frontend/damn-it-app/src/components/News.jsx
+++ app/frontend/damn-it-app/src/components/News.jsx
@@ -1,19 +1,67 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect, useContext} from 'react';
+import {UserConetext} from "../context/UserContext";
 
 const News = () => {
     const [news, setNews] = useState([]);
     const [isModalActive, setModalActive] = useState(false);
     const [selectedNews, setSelectedNews] = useState(null);
+    const [token, setToken] = useContext(UserConetext);
+    const [userId, setUserId] = useState("");
+    const [notification, setNotification] = useState(false);
 
     const toggleModal = (newsItem = null) => {
         setModalActive(!isModalActive);
         setSelectedNews(newsItem);
     };
 
+    const closeModal = () => {
+        setNotification(false);
+    }
+
+    const handleSaveNews = async (news_id) => {
+
+        const requestOptions = {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+                news_id: news_id,
+                user_id: userId
+            })
+        }
+
+        const response = await fetch("/news/save", requestOptions);
+        if (!response.ok) {
+            console.error("Bad request");
+        } else {
+            const data = await response.json()
+            console.log(data.msg);
+            setNotification(true);
+        }
+    }
+
+    const getUser = async () => {
+        const requestOptions = {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: "Bearer " + token,
+            },
+        };
+
+        const response = await fetch("/user/me", requestOptions);
+
+        if (!response.ok) {
+            setToken(null);
+        } else {
+            const data = await response.json();
+            setUserId(data.id);
+        }
+    };
+
     const getNews = async () => {
         const requestOptions = {
             method: 'GET',
-            headers: { "Content-Type": "application/json" },
+            headers: {"Content-Type": "application/json"},
         };
 
         try {
@@ -28,51 +76,59 @@ const News = () => {
 
     useEffect(() => {
         getNews();
+        getUser();
     }, []);
 
     return (
-        <div className="column" id="main">
-            {news.map((el) => (
-                <div key={el.id} className="card">
-                    <h4 className="title is-4">{el.title}</h4>
-                    <div className="mb-5">{el.short_info}</div>
-                    <button
-                        type="button"
-                        className="button is-primary"
-                        onClick={() => toggleModal(el)}
-                    >
-                        Подробнее
-                    </button>
-                </div>
-            ))}
+        <>
+            {notification && (<div className="notification is-info">
+                <button className="delete" onClick={closeModal}></button>
+                Статья сохранена.
+            </div>)}
+            <div className="column" id="main">
+                {news.map((el) => (
+                    <div key={el.id} className="card">
+                        <h4 className="title is-4">{el.title}</h4>
+                        <div className="mb-5">{el.short_info}</div>
+                        <button
+                            type="button"
+                            className="button is-primary"
+                            onClick={() => toggleModal(el)}
+                        >
+                            Подробнее
+                        </button>
+                    </div>
+                ))}
 
-            {isModalActive && selectedNews && (
-                <div className="modal-overlay" onClick={() => toggleModal()}>
-                    <div
-                        className="modal-content animated"
-                        onClick={(e) => e.stopPropagation()}
-                    >
-                        <div className="modal-head">
-                            <h2 className="modal-card-title">{selectedNews.title}</h2>
-                            <button
-                                type="button"
-                                className="btn-close"
-                                onClick={() => toggleModal()}
-                                aria-label="Close"
-                            ></button>
-                        </div>
-                        <div className="modal-body">
-                            {selectedNews.info}
-                        </div>
-                        <div className="modal-footer">
-                            <button type="button" className="button is-info" id="SaveNewsBtn">
-                                Сохранить
-                            </button>
+                {isModalActive && selectedNews && (
+                    <div className="modal-overlay" onClick={() => toggleModal()}>
+                        <div
+                            className="modal-content animated"
+                            onClick={(e) => e.stopPropagation()}
+                        >
+                            <div className="modal-head">
+                                <h2 className="modal-card-title">{selectedNews.title}</h2>
+                                <button
+                                    type="button"
+                                    className="btn-close"
+                                    onClick={() => toggleModal()}
+                                    aria-label="Close"
+                                ></button>
+                            </div>
+                            <div className="modal-body">
+                                {selectedNews.info}
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" onClick={() => handleSaveNews(selectedNews.id)}
+                                        className="button is-info" id="SaveNewsBtn">
+                                    Сохранить
+                                </button>
+                            </div>
                         </div>
                     </div>
-                </div>
-            )}
-        </div>
+                )}
+            </div>
+        </>
     );
 };
 
